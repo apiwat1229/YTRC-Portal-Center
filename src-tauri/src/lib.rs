f5da7068bcfe7d2273fb992ff4482c8dee3d9de7
apiwat sukjareon <inwaui1229@gmail.com>
2025-11-21 13:47:38 +0700

    Create Supllier Page

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use tauri::Builder;
+
+// ===== Commands =====
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -6,9 +10,16 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    Builder::default()
+        // Updater plugin (auto-update)
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        // Process plugin (ใช้ relaunch app หลังอัปเดต)
+        .plugin(tauri_plugin_process::init())
+        // Opener plugin (ที่คุณใช้อยู่)
         .plugin(tauri_plugin_opener::init())
+        // ส่งคำสั่ง Rust ให้ FE เรียกได้
         .invoke_handler(tauri::generate_handler![greet])
+        // run app
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
